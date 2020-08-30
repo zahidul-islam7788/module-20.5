65c80f097032c5b30842e7df15b2c01cf085b8a2
“zahid-islam7788” <zahidul.islam7788@gmail.com>
2020-08-30 21:15:50 +0600

    +amount value done

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const plusBtn = document.getElementById("plus-btn");
 plusBtn.addEventListener('click',function(){
     const amountValue = document.getElementById("amount-value").value;
     const amountNumber = parseFloat(amountValue);
-    
-    const totalPrice = amountNumber + 1;
-    document.getElementById("amount-value").value = totalPrice;
+    const totalAmount = amountNumber + 1;
+    document.getElementById("amount-value").value = totalAmount;
+
+
+    const phonePrice = document.getElementById("phone-price").innerText;
+    const phonePriceNumber = parseFloat(phonePrice);
+    totalPrice = totalAmount * phonePriceNumber;
+    document.getElementById("phone-price").innerText = totalPrice;
 })
